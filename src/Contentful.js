c75077ed7a66730ea20b13a9cb6ad1027487ba10
blackzebi <caominhtri18@gmail.com>
2022-04-26 19:16:25 +0700

    commit to git

---
+++ src/Contentful.js
@@ -0,0 +1,9 @@
+// import {createClient} from 'contentful';
+
+// export default createClient({
+//     space: "",
+//     accessToken: "",
+// })
+
+// coi lại nếu muốn lưu dữ liệu data trên contentful mà ko cần file data hiện tại
+// Bắt đầu từ: 5:16:00
